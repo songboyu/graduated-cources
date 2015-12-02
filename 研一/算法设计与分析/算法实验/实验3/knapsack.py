f643432c27b80c179344a2651e2a55000b4a2674
songboyu <soongboyu@163.com>
2015-12-03 04:32:03 +0800

    algorithm test 3

--- 研一/算法设计与分析/算法实验/实验3/knapsack.py
+++ 研一/算法设计与分析/算法实验/实验3/knapsack.py
@@ -1,7 +1,10 @@
 # coding=utf-8
 
 import random
-
+import time
+import sys   
+# 设置最大递归深度
+sys.setrecursionlimit(10000000)
 
 class Item(object):
 	def __init__(self, w, v):
@@ -14,6 +17,7 @@ class Item(object):
 # 随机生成物品和背包
 def RandomProduce(n, crange):
 	C = random.randint(1, crange)
+	# C = 200
 	items = [None] * n
 	for i in xrange(n):
 		w = random.randint(1, crange)
@@ -29,8 +33,9 @@ def PrintResult(r):
 	for i in xrange(len(r)):
 		u += items[r[i]].w
 		v += items[r[i]].v
-		print items[r[i]]
+		print i,items[r[i]]
 	print '物品总重：'+str(u),'物品价值：'+str(v)
+
 # 动态规划算法
 def DynamicProgramming(items, C):
 	def Dynamic(C):
@@ -51,7 +56,7 @@ def DynamicProgramming(items, C):
 		else:
 			m[0][C] = max(m[1][C], m[1][C-items[0].w]+items[0].v)
 		return m
-
+	# 构造最优解
 	def MakeResult(m, i, j):
 		if i == len(items)-1:
 			return r
@@ -66,30 +71,37 @@ def DynamicProgramming(items, C):
 	MakeResult(m, 0, C)
 	return r
 
+# 近似算法
 def FPTASApproximation(items, C):
 	n = len(items)
-	e = random.uniform(0, 1)
-	P = 0
-	for i in items:
-		if i.v > P:
-			P = i.v
-	K = float(e * P) / n
+	# e = random.uniform(0, 1)
+	e = 2
+	K = float(e * C) / n
+	C = int(C / K)
+	print '背包容量：',str(C)
 	print 'K =',str(K)
 	for i in items:
-		i.v = int(i.v / K)
+		i.w = int(i.w / K)
 	r = DynamicProgramming(items, C)
 	return r
 
 if __name__ == '__main__':
 	items, C = RandomProduce(100,1000)
 	print '背包容量：'+str(C)
-	# 动态规划算法
+	# 动态规划算法1
 	print '-----------动态规划算法-----------'
+	s = time.time()
 	r = DynamicProgramming(items, C)
+	e = time.time()
+	print (e - s)*1000,'ms'
 	PrintResult(r)
+	print
 
 	# 近似算法
 	items = [i for i in items]
 	print '-----------近似算法-----------'
+	s = time.time()
 	r = FPTASApproximation(items, C)
+	e = time.time()
+	print (e - s)*1000,'ms'
 	PrintResult(r)
