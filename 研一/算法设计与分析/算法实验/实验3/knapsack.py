357e5f8a769f9cc4605efe37a316fe19beb26bee
songboyu <soongboyu@163.com>
2015-12-04 17:40:04 +0800

    update 算法实验3

--- 研一/算法设计与分析/算法实验/实验3/knapsack.py
+++ 研一/算法设计与分析/算法实验/实验3/knapsack.py
@@ -2,9 +2,7 @@
 
 import random
 import time
-import sys   
-# 设置最大递归深度
-sys.setrecursionlimit(10000000)
+import sys
 
 class Item(object):
 	def __init__(self, w, v):
@@ -34,64 +32,120 @@ def PrintResult(r):
 		u += items[r[i]].w
 		v += items[r[i]].v
 		print i,items[r[i]]
-	print '物品总重：'+str(u),'物品价值：'+str(v)
+	print u'物品总重：'+str(u),u'物品价值：'+str(v)
 
-# 动态规划算法
-def DynamicProgramming(items, C):
+# 动态规划算法1
+def DynamicProgramming1(items, C):
 	def Dynamic(C):
-		n = len(items)
 		m = [([None] * (C+1)) for row in xrange(n)]
 
-		for j in xrange(min(items[n-1].w-1, C)+1):
-			m[n-1][j] = 0
-		for j in xrange(items[n-1].w, C+1):
-			m[n-1][j] = items[n-1].v
-		for i in reversed(list(xrange(1, n-1))):
-			for j in xrange(min(items[i].w-1, C)+1):
-				m[i][j] = m[i+1][j]
-			for j in xrange(items[i].w, C+1):
-				m[i][j] = max(m[i+1][j], m[i+1][j-items[i].w]+items[i].v)
-		if C < items[0].w:
-			m[0][C] = m[1][C]
-		else:
-			m[0][C] = max(m[1][C], m[1][C-items[0].w]+items[0].v)
+		for j in xrange(C+1):
+			if j < items[n-1].w:
+				m[n-1][j] = 0
+			else:
+				m[n-1][j] = items[n-1].v
+		for i in reversed(list(xrange(0, n-1))):
+			for j in xrange(C+1):
+				if j < items[i].w:
+					m[i][j] = m[i+1][j]
+				else:
+					m[i][j] = max(m[i+1][j], m[i+1][j-items[i].w]+items[i].v)
 		return m
 	# 构造最优解
-	def MakeResult(m, i, j):
-		if i == len(items)-1:
-			return r
-		if m[i][j] == m[i+1][j]:
-			MakeResult(m, i+1, j)
-		else :
-			r.append(i)
-			MakeResult(m, i+1, j-items[i].w)
+	def MakeResult(m):
+		j = C
+		for i in xrange(n-1):
+			if m[i][j] != m[i+1][j]:
+				r.append(i)
+				j = j - items[i].w
+		if m[n-1][j] != 0:
+			r.append(n-1)
 
+	n = len(items)
 	m = Dynamic(C)
 	r = []
-	MakeResult(m, 0, C)
+	MakeResult(m)
+	return r
+
+# 动态规划算法2
+def DynamicProgramming2(items, C):
+	def Dynamic(Vsum):
+		m = [([None] * (Vsum+1)) for row in xrange(n)]
+
+		for j in xrange(Vsum+1):
+			if j == 0:
+				m[n-1][j] = 0
+			elif j > items[n-1].v:
+				m[n-1][j] = float("inf")
+			else:
+				m[n-1][j] = items[n-1].w
+		for i in reversed(list(xrange(0, n-1))):
+			for j in xrange(Vsum+1):
+				if j < items[i].v:
+					m[i][j] = m[i+1][j]
+				else:
+					m[i][j] = min(m[i+1][j], m[i+1][j-items[i].v]+items[i].w)
+		return m
+	# 构造最优解
+	def MakeResult(m):
+		j = s
+		for i in xrange(n-1):
+			if m[i][j] != m[i+1][j]:
+				r.append(i)
+				j = j - items[i].v
+		if m[n-1][j] != float("inf") and m[n-1][j] != 0:
+			r.append(n-1)
+
+	# 找到矩阵m中可用的最大V（容量小于C）
+	def FindAvailableMaxV(m):
+		for j in reversed(xrange(Vsum+1)):
+			for i in xrange(n):
+				if m[i][j] <= C:
+					return j
+	Vsum = 0
+	for i in items:
+		Vsum += i.v
+
+	n = len(items)
+	m = Dynamic(Vsum)
+
+	s = FindAvailableMaxV(m)
+	r = []
+	MakeResult(m)
 	return r
 
 # 近似算法
 def FPTASApproximation(items, C):
+	Vmax = 0
+	for i in items:
+		if i.v > Vmax:
+			Vmax = i.v
 	n = len(items)
 	# e = random.uniform(0, 1)
-	e = 2
-	K = float(e * C) / n
-	C = int(C / K)
-	print '背包容量：',str(C)
-	print 'K =',str(K)
+	e = 0.9
+	K = n / e
+	print u'K =',str(K)
 	for i in items:
-		i.w = int(i.w / K)
-	r = DynamicProgramming(items, C)
+		i.v = int(i.v * ( K / Vmax ))
+	r = DynamicProgramming2(items, C)
 	return r
 
 if __name__ == '__main__':
 	items, C = RandomProduce(100,1000)
-	print '背包容量：'+str(C)
+	print u'背包容量：'+str(C)
 	# 动态规划算法1
-	print '-----------动态规划算法-----------'
+	print u'-----------动态规划算法1-----------'
 	s = time.time()
-	r = DynamicProgramming(items, C)
+	r = DynamicProgramming1(items, C)
+	e = time.time()
+	print (e - s)*1000,'ms'
+	PrintResult(r)
+	print
+
+	# 动态规划算法2
+	print u'-----------动态规划算法2-----------'
+	s = time.time()
+	r = DynamicProgramming2(items, C)
 	e = time.time()
 	print (e - s)*1000,'ms'
 	PrintResult(r)
@@ -99,7 +153,7 @@ if __name__ == '__main__':
 
 	# 近似算法
 	items = [i for i in items]
-	print '-----------近似算法-----------'
+	print u'-----------近似算法-----------'
 	s = time.time()
 	r = FPTASApproximation(items, C)
 	e = time.time()
